b92f2eeca308461c85a138279a3ed4278e97cddb
Ryoung27 <rryoung89@gmail.com>
2023-03-02 19:57:05 -0600

    correct dsn

--- config/Database.php
+++ config/Database.php
@@ -22,7 +22,8 @@
                 //connection already exists, return it.
                 return $this->conn;
             } else{
-                $dsn = "postgres://{$this->username}:{$this->password}@{$this->host}" . ".oregon-postgres.render.com/{$this->dbname};";
+                //xxx.oregon-postgres.render.com all the way to render.com
+                $dsn = 'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->dbname;
 
                 try {
                     $this->conn = new PDO($dsn, $this->username, $this->password);
