8901ecd28d8df20ea944051b9094d76977a43278
Ryoung27 <rryoung89@gmail.com>
2023-03-03 15:54:25 -0600

    remove html

--- api/authors/index.php
+++ api/authors/index.php
@@ -8,19 +8,9 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quotes MidTerm</title>
-</head>
-<body>
-    <h1>
-        Authors
-    </h1>
-</body>
-</html>
+if($method === 'GET'){
+    include_once 'read.php';
+}
+
+?>
--- api/categories/index.php
+++ api/categories/index.php
@@ -9,18 +9,3 @@ if ($method === 'OPTIONS') {
     exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quotes MidTerm</title>
-</head>
-<body>
-    <h1>
-        Categories
-    </h1>
-</body>
-</html>
