52ec41161f610f874ddd0fe48144ded81d13b600
Ryoung27 <rryoung89@gmail.com>
2023-03-05 16:43:44 -0600

    read_single for category

---
+++ api/categories/read_single.php
@@ -0,0 +1,25 @@
+<?php
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate category object
+    $category = new Category($db);
+
+    // GET ID
+    $category->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    //Get post
+    $author->read_single();
+
+    //Create array
+    $post_arr = array(
+        'id'            => $author->id,
+        'category'      => $category->category,
+    );
+
+    // Make JSON
+    print_r(json_encode($post_arr))
