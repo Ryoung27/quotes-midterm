fbef90219e7f1f899e190994427dd791f0fd277b
Ryoung27 <rryoung89@gmail.com>
2023-03-05 16:39:14 -0600

    limit 1 not limit 0, 1, in Author model

--- models/Author.php
+++ models/Author.php
@@ -41,7 +41,7 @@
                 ' . $this->table . '
                 WHERE
                 id = ?
-            LIMIT 0,1';
+            LIMIT 1';
 
             //Prepare statement
             $stmt = $this->conn->prepare($query);
