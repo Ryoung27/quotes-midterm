b936a76614f8f79c83cea356efff22ed8e8d84a7
Ryoung27 <rryoung89@gmail.com>
2023-03-05 18:30:55 -0600

    update category to use category variable

--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -12,14 +12,14 @@
     // GET ID
     $category->id = isset($_GET['id']) ? $_GET['id'] : die();
 
-    //Get post
+    //Get category
     $category->read_single();
 
     //Create array
-    $post_arr = array(
+    $category_arr = array(
         'id'            => $category->id,
         'category'      => $category->category,
     );
 
     // Make JSON
-    print_r(json_encode($post_arr));
+    print_r(json_encode($category_arr));
