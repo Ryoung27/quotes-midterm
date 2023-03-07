2013311b976892d45f118d1bfbdcf81c347da1a3
Ryoung27 <rryoung89@gmail.com>
2023-03-07 13:48:33 -0600

    delete for quotes

---
+++ api/quotes/delete.php
@@ -0,0 +1,31 @@
+<?php
+    include_once '../../config/Database.php';
+    include_once '../../models/Quote.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate quote object
+    $quote = new Quote($db);
+
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    // Set ID to update
+    $quote->id = $data->id;
+
+    //Delete quote
+    if($quote->delete()){
+        //Create array
+        $quote_arr = array(
+            'id'            => $quote->id,
+        );
+
+        // Make JSON
+        print_r(json_encode($quote_arr));
+    }else{
+        echo json_encode(
+            array('message' => 'Quote Not Deleted')
+        );  
+    }
