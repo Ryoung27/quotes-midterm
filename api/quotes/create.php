5aa8b6575d4a716ee97809b9a625995129358b01
Ryoung27 <rryoung89@gmail.com>
2023-03-07 19:20:14 -0600

    guard quotes

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -14,25 +14,31 @@
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
-    $quote->quote = $data->quote;
-    $quote->author_id = $data->author_id;
-    $quote->category_id = $data->category_id;
-    $quote_id = $quote->create();
+    $quote->quote = isset($data->quote) ? $data->quote : null;
+    $quote->author_id = isset($data->author_id) ? $data->author_id : null;
+    $quote->category_id = isset($data->category_id) ? $data->author_id : null;
+    if(isset($quote->quote) && isset($quote->author_id) && isset($quote->category_id)){
+        $quote_id = $quote->create();
 
-    //Create quote
-    if($quote_id){
-        //Create array
-        $quote_arr = array(
-            'id'            => $quote_id,
-            'quote'         => $quote->quote,
-            'author_id'     => $quote->author_id,
-            'category_id'     => $quote->category_id,
-        );
+        //Create quote
+        if($quote_id){
+            //Create array
+            $quote_arr = array(
+                'id'            => $quote_id,
+                'quote'         => $quote->quote,
+                'author_id'     => $quote->author_id,
+                'category_id'   => $quote->category_id,
+            );
 
-        // Make JSON
-        print_r(json_encode($quote_arr));
+            // Make JSON
+            print_r(json_encode($quote_arr));
+        }else{
+            echo json_encode(
+                array('message' => 'Quote Not Created')
+            );  
+        }
     }else{
         echo json_encode(
-            array('message' => 'Quote Not Created')
-        );  
+            array('message' => 'Missing Required Parameters')
+        ); 
     }
