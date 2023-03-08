890e05c28fafbf5590c12221241efa2518767baf
Ryoung27 <rryoung89@gmail.com>
2023-03-07 19:10:46 -0600

    guard against missing catagory params

--- api/authors/create.php
+++ api/authors/create.php
@@ -15,21 +15,20 @@
     $data = json_decode(file_get_contents("php://input"));
 
     $author->author = isset($data->author) ? $data->author : null;
+
     if(isset($author->author)){
-    $author_id = $author->create();
-    //Create author
-    if($author_id && $author->author){
-        //Create array
-        $author_arr = array(
-            'id'            => $author_id,
-            'author'        => $author->author,
-        );
+        $author_id = $author->create();
+        //Create author
+        if($author_id && $author->author){
+            //Create array
+            $author_arr = array(
+                'id'            => $author_id,
+                'author'        => $author->author,
+            );
 
-        // Make JSON
-        print_r(json_encode($author_arr));
-    }
-
-    
+            // Make JSON
+            print_r(json_encode($author_arr));
+        }
     }else{
         echo json_encode(
             array('message' => 'Missing Required Parameters')
--- api/categories/create.php
+++ api/categories/create.php
@@ -14,21 +14,24 @@
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
-    $category->category = $data->category;
-    $category_id = $category->create();
+    $category->category = isset($data->category) ? $data->category : null;
 
-    //Create category
-    if($category_id){
-        //Create array
-        $category_arr = array(
-            'id'              => $category_id,
-            'category'        => $category->category,
-        );
 
-        // Make JSON
-        print_r(json_encode($category_arr));
+    if(isset($category->category)){
+        $category_id = $category->create();
+
+        //Create category
+        if($category_id){
+            //Create array
+            $category_arr = array(
+                'id'              => $category_id,
+                'category'        => $category->category,
+            );
+
+            // Make JSON
+            print_r(json_encode($category_arr));
     }else{
         echo json_encode(
-            array('message' => 'Category Not Created')
-        );  
+            array('message' => 'Missing Required Parameters')
+        ); 
     }
