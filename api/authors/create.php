52ee8c31f41b5e19e10efde83cae1ef24513d27a
Ryoung27 <rryoung89@gmail.com>
2023-03-07 19:05:27 -0600

    guard authors create

--- api/authors/create.php
+++ api/authors/create.php
@@ -14,11 +14,11 @@
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
-    $author->author = $data->author;
+    $author->author = isset($data->author) ? $data->author : null;
+    if(isset($author->author)){
     $author_id = $author->create();
-
     //Create author
-    if($author_id){
+    if($author_id && $author->author){
         //Create array
         $author_arr = array(
             'id'            => $author_id,
@@ -27,8 +27,11 @@
 
         // Make JSON
         print_r(json_encode($author_arr));
+    }
+
+    
     }else{
         echo json_encode(
-            array('message' => 'Author Not Created')
+            array('message' => 'Missing Required Parameters')
         );  
     }
